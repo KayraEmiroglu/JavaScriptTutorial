04a3a1b3f48d58cc2ad387830b833645ce954f3e
Kayra Emiroglu <kemiroglu7@gmail.com>
2022-12-23 23:44:43 +0300

    Day15

--- practice/app16.js
+++ practice/app16.js
@@ -36,3 +36,37 @@ console.log(birlesikDizi);
 let ulkeler1 =["Türkiye","Almanya","Fransa","Hollanda","İtalya","İspanya","Portekiz"];
 
 let yeniDizi = [] ;
+
+////7- Dizideki notların ortalamasını bulun, ortalamadan yuksek olanların 
+//ortalamasını %20, düşük olanların ortalamasını %10 artırarak yeni diziye atalım.
+
+const notlar = [
+  { ad: "Ali Can", not: 38 },
+  { ad: "Veli Kan", not: 62 },
+  { ad: "Oya Tan", not: 74 },
+  { ad: "Elif Han", not: 28 },
+  { ad: "Mine San", not: 86 },
+  { ad: "Mert Er", not: 67 },
+  { ad: "Bora Fer", not: 92 },
+  { ad: "Ayşe Yılmaz", not: 100,},
+  { ad: "İlke Cansız", not: 56,},
+  { ad: "Sude Canlı", not: 94 },
+  { ad: "Bade Sert", not: 72,},
+  ];
+
+let ort = notlar.reduce((x,y)=> x + y.not,0);
+ort = (ort/notlar.length).toFixed(2);
+console.log(ort);
+
+const yeniNotlar = notlar.map(item=>{
+
+  if(item.not>=ort){
+    item.not = (item.not*1.2).toFixed(2);
+  }else{
+    item.not = (item.not*1.1).toFixed(2);
+  }
+
+  return item;
+});
+
+console.log(yeniNotlar);
